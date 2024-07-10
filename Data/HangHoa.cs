e589bc0821c93566db214151ea7222ef8dc60a42
lekhuyen <127919257+lekhuyen@users.noreply.github.com>
2024-07-10 16:04:00 +0700

    Add project files.

---
+++ Data/HangHoa.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APIB2.Data
+{
+    public class HangHoa
+    {
+        [Key]
+        public Guid MaHh { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string TenHh { get; set; }
+        public string MoTa { get; set; }
+
+        [Range(0, double.MaxValue)]
+        public double DonGia { get; set; }
+        public byte GiamGia { get; set; }
+    }
+}
---
+++ Data/MyDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace APIB2.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<HangHoa> HangHoas { get; set; }
+    }
+}
